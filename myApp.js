a041603300a85eb71883715fb449890dbb43df5c
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-05-22 20:51:32 +0300

    Add mongo db connection, and create person schema and model

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
+let mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+let Schema = mongoose.Schema;
 
-let Person;
+let PersonSchema = new Schema({  
+  name: {type: String, required: true},
+  age: {type: Number},
+  favoriteFoods : [String]
+});
+let Person = mongoose.model("Person", PersonSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
